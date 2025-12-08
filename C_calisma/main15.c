208bd474ded7852781394980989bf4b7faac4ef1
Efdal Gözübüyük <efdalgozubuyuk@gmail.com>
2025-12-08 22:52:29 +0300

    Add files via upload

--- C_calisma/main15.c
+++ C_calisma/main15.c
@@ -3,14 +3,16 @@
 
 void main(){
 	
-	int i,j;
+	int i,j=0;
 	
 	for(i=100;i<200;i++){
 		if(i%9==0){
 			printf("%d sayisi 9 a tam bolunuyor.\n",i);
+			j+=i;
 		}else{
 			printf("%d sayisi 9 a tam bolunmuyor.\n",i);
 		}
 	}
+	printf("100-200 arasi 9 bolunenlerin toplami = %d",j);
 	
 }
